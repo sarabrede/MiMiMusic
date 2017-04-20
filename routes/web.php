58c1627fea8c4923e992514663afad48cc9c49ff
Sara Brede <sara_brede@hotmail.com>
2017-04-20 01:55:13 -0500

    Agregué la ruta de búsqueda

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,11 +9,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('landingPage');
 });
-
 Route::get('/index', function () {
     return view('index');
+});
+
+Route::get('/search', function () {
+return view('search');
+
 });
