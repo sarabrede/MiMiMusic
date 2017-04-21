df8c0f0aa72febf56535ed5d55d35435043f7687
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-20 21:16:48 -0500

    Update searchComponent.blade.php

--- resources/views/searchComponent.blade.php
+++ resources/views/searchComponent.blade.php
@@ -1,17 +1,17 @@
 <div class ="col-xs-11 SearchResult">
 	<div class="col-xs-5 imageFrame">
-		<img src="audios/AlbumImages/09.jpg" width="100%" height="100%" class="img-rounded"/>
+		<img src="{{ $image }}" width="100%" height="100%" class="img-rounded"/>
 	</div>
 				
 	<div class="col-xs-4 infoResult">
-		<a> Título de la canción </a> 
+		<a> {{ $title }} </a> 
 		<br>
-		<a> Título albúm </a>
+		<a> {{ $album }} </a>
 		<br>
-		<a> nombreUsuario </a>
-		<p> descripción </p>
+		<a> {{ $author }} </a>
+		<p> {{ $description }} </p>
 	</div>
 	<div class="col-xs-1 infoResult">
-		<p> $10.0 </p>
+		<p> {{ ($price > 0) ? 'US$'.$price : 'Free'}} </p>
 	</div>
 </div>
