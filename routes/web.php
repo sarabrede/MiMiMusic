b539ac218c32908080682e60b3fd8fd39ba8e36a
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-20 21:06:53 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,14 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('landingPage');
 });
+
 Route::get('/index', function () {
     return view('index');
 });
 
-Route::get('/search', function () {
-return view('search');
+Route::get('/search/{searchParam?}', function($searchParam = '') {
+	$songs = DB::table('Cancion')
+	->join('Album', 'Cancion.idAlbum', '=', 'Album.idAlbum')
+	->join('Usuario', 'Usuario.idUsuario', '=', 'Album.idUsuario')
+	->select('Cancion.*', 'Album.tituloAlbum', 'Album.fotoAlbum', 'Album.precio', 'Usuario.nombreUsuario')
+	->where('Cancion.tituloCancion', 'like', '%'.$searchParam.'%')
+	->orderby('fechaPublicacion', 'desc')
+	->limit(20)
+	->get();
+	return view('search', ['songs' => $songs, 'search' =>$searchParam]);
+});
 
+Route::get('/shop', function() {
+	$albums = DB::table('Album')
+	->join('Cancion', 'Cancion.idAlbum', '=', 'Album.idAlbum')
+	->join('Usuario', 'Usuario.idUsuario', '=', 'Album.idUsuario')
+	->select('Album.*', 'Usuario.nombreUsuario', DB::raw("count(Cancion.tituloCancion) as count"))
+	->groupby('Album.idAlbum', 'Album.fotoAlbum', 'Album.añoAlbum', 'Album.tituloAlbum', 'Album.idUsuario', 'Album.activo', 'Album.precio', 'Usuario.nombreUsuario')
+	->get();
+    return view('cart', ['albums' => $albums]);
+});
+
+Route::get('/profile', function () {
+    return view('profile');
 });
