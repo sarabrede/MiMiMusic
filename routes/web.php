ee8365fd4f951e9323f40ec96eb5304cc45b84d4
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-22 03:23:12 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,36 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('landingPage');
-});
+Route::get('/', 'SongController@landingSongs');
 
-Route::get('/index', function () {
-    return view('index');
-});
+Route::get('/index/{type?}/{idUser?}', 'SongController@indexSong');
 
-Route::get('/search/{searchParam?}', function($searchParam = '') {
-	$songs = DB::table('Cancion')
-	->join('Album', 'Cancion.idAlbum', '=', 'Album.idAlbum')
-	->join('Usuario', 'Usuario.idUsuario', '=', 'Album.idUsuario')
-	->select('Cancion.*', 'Album.tituloAlbum', 'Album.fotoAlbum', 'Album.precio', 'Usuario.nombreUsuario')
-	->where('Cancion.tituloCancion', 'like', '%'.$searchParam.'%')
-	->orderby('fechaPublicacion', 'desc')
-	->limit(20)
-	->get();
-	return view('search', ['songs' => $songs, 'search' =>$searchParam]);
-});
+Route::get('/search/{searchParam?}/{type?}', 'SongController@searchSong');
 
-Route::get('/shop', function() {
-	$albums = DB::table('Album')
-	->join('Cancion', 'Cancion.idAlbum', '=', 'Album.idAlbum')
-	->join('Usuario', 'Usuario.idUsuario', '=', 'Album.idUsuario')
-	->select('Album.*', 'Usuario.nombreUsuario', DB::raw("count(Cancion.tituloCancion) as count"))
-	->groupby('Album.idAlbum', 'Album.fotoAlbum', 'Album.añoAlbum', 'Album.tituloAlbum', 'Album.idUsuario', 'Album.activo', 'Album.precio', 'Usuario.nombreUsuario')
-	->get();
-    return view('cart', ['albums' => $albums]);
-});
+Route::get('/shop', 'AlbumController@shopAlbum');
 
-Route::get('/profile', function () {
-    return view('profile');
-});
+Route::get('/profile/{userProfile}', 'UserController@profileUser');
