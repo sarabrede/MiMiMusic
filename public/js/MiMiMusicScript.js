8a91ab4fbfc1fe97dd1d4ccbd7207ae7fb420937
Sara Brede <sara_brede@hotmail.com>
2017-04-22 02:10:41 -0500

    Update MiMiMusicScript.js

--- public/js/MiMiMusicScript.js
+++ public/js/MiMiMusicScript.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
+	/*Perfil*/ 
+	
+	/*Mover la página de perfil un poco, más abajo del banner*/
+	var totalToMove = ($("#ProfilePage").height()) / 5;
 
-	//$(".formSignUp").hide();
+	$("#ProfilePage").scrollTop(totalToMove);
 
-	$(".formLogIn p").click(function(){
-		$(".formSignUp").show();
-		$(".formLogIn").hide();
+	/*Hover para que aparezca y desaparezca la camarita en la foto de perfil*/
+	$(".profilePicture").mouseover(function(){
+		$(".inputPhotoProfile").addClass("hoverOverPhoto");
 	});
 
+	$(".profilePicture").mouseout(function(){
+		$(".inputPhotoProfile").removeClass("hoverOverPhoto");
+	});
 });
