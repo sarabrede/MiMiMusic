166bba19b351a27c5375d64c63aa1460154bb3cf
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-22 03:15:10 -0500

    Update amazingaudioplayer.blade.php

--- resources/views/amazingaudioplayer.blade.php
+++ resources/views/amazingaudioplayer.blade.php
@@ -1,10 +1,9 @@
-
 <div class="col-xs-4">
 	<div class="thumbnail">
 		<div class="caption">
 		    <div class="amazingaudioplayer" style="display:block;position:relative;width:100%;height:auto;margin:0px auto 0px;">
 		        <ul class="amazingaudioplayer-audios" style="display:none;">
-		            <li data-artist="{{ $author }}" data-title="{{ $title }}" data-album="{{ $album }}" data-info="{{ $description }}" data-image="{{ $image }}" data-duration="10">
+		            <li data-artist="{{ $author }}" data-title="{{ $title }}" data-album="{{ $album }}" data-info="{{ $description }}" data-image="{{ asset($image) }}" data-duration="10">
 		                <div class="amazingaudioplayer-source" data-src="{{ $source }}" data-type="audio/mpeg" />
 		            </li>
 		        </ul>
