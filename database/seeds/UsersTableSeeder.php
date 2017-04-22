3350af7a1b5f99339d50445d5284b9372c78335a
Sara Brede <sara_brede@hotmail.com>
2017-04-22 02:14:04 -0500

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,16 +12,20 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('Usuario')->insert([
+            'idUsuario' => '1',
         	'nombreUsuario' => 'sarabrede',
         	'correoElectronico' => 'sara_brede@hotmail.com',
         	'contraseña' => 'uwu',
+            'nombreCompleto' => 'Sara Victoria Brede Treviño', 
         	'fechaCreacion' => \Carbon\Carbon::now()
         	]);
 
         DB::table('Usuario')->insert([
+            'idUsuario' => '2',
         	'nombreUsuario' => 'EdgarMtz1807',
         	'correoElectronico' => 'ALFREDO_950207@hotmail.com',
         	'contraseña' => 'si',
+            'nombreCompleto' => 'Edgar Alfredo Martínez Monjarás', 
         	'fechaCreacion' => \Carbon\Carbon::now()
         	]);
     }
