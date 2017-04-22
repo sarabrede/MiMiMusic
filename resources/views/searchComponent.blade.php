3beabea58f6677fba726ce8a494490dece4fd6c6
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-22 03:19:42 -0500

    Update searchComponent.blade.php

--- resources/views/searchComponent.blade.php
+++ resources/views/searchComponent.blade.php
@@ -1,14 +1,16 @@
 <div class ="col-xs-11 SearchResult">
 	<div class="col-xs-5 imageFrame">
-		<img src="{{ $image }}" width="100%" height="100%" class="img-rounded"/>
+		<img src="{{ asset($image) }}" width="100%" height="100%" class="img-rounded"/>
 	</div>
 				
 	<div class="col-xs-4 infoResult">
-		<a> {{ $title }} </a> 
+		<a href="{{ url('song') }}/{{ $id }}" target="_blank"> {{ $title }} </a> 
 		<br>
-		<a> {{ $album }} </a>
+		<a href="{{ url('album') }}/{{ $idAlbum }}" target="_blank"> {{ $album }} </a>
 		<br>
-		<a> {{ $author }} </a>
+		<label>{{ $genre }}</label>
+		<br>
+		<a href="{{ url('profile') }}/{{ $idUser }}" target="_blank"> {{ $author }} </a>
 		<p> {{ $description }} </p>
 	</div>
 	<div class="col-xs-1 infoResult">
