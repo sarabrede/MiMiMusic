1a55b40b9613ec4174b217d5da60f0d34eae439c
Sara Brede <sara_brede@hotmail.com>
2017-04-23 16:54:06 -0500

    Update amazingaudioplayer.blade.php

--- resources/views/amazingaudioplayer.blade.php
+++ resources/views/amazingaudioplayer.blade.php
@@ -1,7 +1,7 @@
-<div class="col-xs-4">
+<div class="col-sm-4 col-xs-12">
 	<div class="thumbnail">
 		<div class="caption">
-		    <div class="amazingaudioplayer" style="display:block;position:relative;width:100%;height:auto;margin:0px auto 0px;">
+		    <div class="amazingaudioplayer " style="display:block;position:relative;width:100%;height:auto;margin:0px auto 0px;">
 		        <ul class="amazingaudioplayer-audios" style="display:none;">
 		            <li data-artist="{{ $author }}" data-title="{{ $title }}" data-album="{{ $album }}" data-info="{{ $description }}" data-image="{{ asset($image) }}" data-duration="10">
 		                <div class="amazingaudioplayer-source" data-src="{{ $source }}" data-type="audio/mpeg" />
