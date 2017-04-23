d900aba31b30a2b4d97121970202b8011e7caa46
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-23 03:03:00 -0500

    Update songComponent.blade.php

--- resources/views/songComponent.blade.php
+++ resources/views/songComponent.blade.php
@@ -1,8 +1,19 @@
-<div class="col-xs-12 songContainer ">
-	<div class="col-xs-9 nameSong">
-		<a href="#"> Nombre de la canción </a>
+@if( $idSong == $id)
+	<div class="col-xs-12 songContainer activeSong">
+		<div class="col-xs-9 nameSong">
+			<a href="{{ url('song') }}/{{ $id }}" target="_blank"> {{ $title }} </a>
+		</div>
+		<div class="col-xs-2 pull-right timeSong">
+			<p> 3:50 </p>
+		</div>
 	</div>
-	<div class="col-xs-2 pull-right timeSong">
-		<p> 3:50 </p>
+@else
+	<div class="col-xs-12 songContainer ">
+		<div class="col-xs-9 nameSong">
+			<a href="{{ url('song') }}/{{ $id }}" target="_blank"> {{ $title }} </a>
+		</div>
+		<div class="col-xs-2 pull-right timeSong">
+			<p> 3:50 </p>
+		</div>
 	</div>
-</div>
+@endif
