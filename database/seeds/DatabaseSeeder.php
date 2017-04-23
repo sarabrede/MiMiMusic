4ce42e299b7f9923be2fd53e88ad9df07197c4c9
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-22 20:42:10 -0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(PaisTableSeder::class);
     	$this->call(UsersTableSeeder::class);
     	$this->call(AlbumTableSeeder::class);
     	$this->call(GeneroTableSeeder::class);
