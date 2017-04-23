20e010ce75e70b6ed9cf5e37eb760368d1127843
EdgarMtz1807 <ALFREDO_950207@hotmail.com>
2017-04-22 20:38:56 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,25 @@ class UserController extends Controller
     public function profileUser($user)
     {
 		$info = DB::table('Usuario')
-		->select('Usuario.*')
+		->join('Pais', 'Usuario.idPais', '=', 'Pais.idPais')
+		->select('Usuario.*', 'Pais.nombrePais')
 		->where('Usuario.idUsuario', '=', $user)
+		->first();
+		$songs = $this->songsUser($user);
+        return view('profile', ['info' => $info, 'songs' => $songs]);
+    }
+
+    public function songsUser($user)
+    {
+    	$songs = DB::table('Cancion')
+		->join('Album', 'Cancion.idAlbum', '=', 'Album.idAlbum')
+		->join('Usuario', 'Usuario.idUsuario', '=', 'Album.idUsuario')
+		->join('Genero', 'Genero.idGenero', '=', 'Cancion.idGenero')
+		->select('Cancion.*', 'Album.idAlbum', 'Album.tituloAlbum', 'Album.fotoAlbum', 'Album.precio', 'Usuario.nombreUsuario', 'Usuario.idUsuario', 'Genero.nombreGenero')
+		->where('Usuario.idUsuario', '=', $user)
+		->orderby('fechaPublicacion', 'desc')
+		->limit(20)
 		->get();
-        return view('profile', ['info' => $info]);
+        return $songs;
     }
 }
