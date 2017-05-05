bd5eeb8b0c81b9955f5075cba6e9e8bf3db6fd99
Sara Brede <sara_brede@hotmail.com>
2017-05-05 13:40:47 -0500

    Update landingPage.blade.php

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -18,6 +18,7 @@
 					</div>
 
 					<div class="row">
+
 							<form class="formLogIn">
 								<div class="col-xs-12 rowLogin">
 								<div class="col-xs-12">
@@ -40,10 +41,45 @@
 
 								<div class="col-xs-12">
 									<input type="submit" class="btn btn-lg btnLogin" value="Log in"/>
-								<p> Are you new here? <u> Sign up now! </u></p>
+								<p id="newHere"> Are you new here? <u> Sign up now! </u></p>
 								</div>
 								</div>
 							</form>
+
+							<form class="formSignUp">
+							<div class="col-xs-12 rowLogin">
+							<div class="col-xs-12">
+								<label class="headerText"> Sign Up </label> 
+							</div>
+
+							<div class="col-xs-12">
+									<div class="form-group form-group-lg row">
+									<label for="emailInput" class="pull-left">Email:</label>
+									<input type="email" class="form-control" id="emailInput" placeholder="Email"/>
+						</div>
+						</div>
+
+						<div class="col-xs-12">
+						<div class="form-group form-group-lg row">
+						<label for="usernameInput" class="pull-left">Username:</label>
+							<input type="text" class="form-control" id="usernameInput" placeholder="Username" />
+						</div>
+						</div>
+
+						<div class="col-xs-12">
+						<div class="form-group form-group-lg row">
+						<label for="passwordInput" class="pull-left">Password:</label>
+							<input type="password" class="form-control" id="passwordInput" placeholder="Password" />
+						</div>
+						</div>
+
+						<div class="col-xs-12">
+						<input type="submit" class="btn btnLogin btnSignUp" value="Sign Up"/>
+						<br>
+						<p id="account"> <u> I already have an account! </u> </p>
+						</div>
+						</div>
+					</form>
 						
 						
 					</div>
@@ -52,13 +88,13 @@
 					
 				</div>
 				<div class="col-sm-8 col-xs-12 imagePanel">
-					<div class="panel panel-default musicPanel">
+					<div class="panel panel-default musicPanel" id="landingPanel">
 						<div class="panel-body">
 							<form class="form-group">
 							  <div class="input-group">
 							    <input type="text" class="form-control" placeholder="Search..."/>
 							    <div class="input-group-btn">
-							      <button class="btn btn-default" type="submit">
+							      <button class="btn btn-default" type="button">
 							        <i class="glyphicon glyphicon-search"></i>
 							      </button>
 							    </div>
