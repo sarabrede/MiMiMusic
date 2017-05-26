7fe768811d24d54682efc05b722dce786ef3c2ae
Sara Brede <sara_brede@hotmail.com>
2017-05-25 23:11:58 -0500

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,9 +10,11 @@
 		<div class="panel-body">
 			<div class="row searchRow">
 				<div class ="col-xs-8 SearchPanel">
-					<div class ="col-xs-3">
+					
+					{{--<div class ="col-xs-3">
 						<button type="button" class="btn btnFilter" style="width: 100%"> Filter <span class="caret"> </span> </button>
-					</div>
+					</div>--}}
+					
 					@if(sizeof($songs) > 0)
 
 					@foreach ($songs as $song)
@@ -21,16 +23,16 @@
 					@endforeach
 
 					@else
-						<p> Sorry, baka, we didn't find anything. It's not like we care or anything. </p>
+						<p> Sorry, we didn't find anything. </p>
 					@endif
 
 					
 				</div>
 				<div class="col-xs-4 SearchPills">
 			    	<ul class="nav nav-tabs nav-stacked">
-			        	<li><a href="{{ url('search') }}/song/{{ $search }}" class="songsSearch activePill" >Songs</a></li>
-			        	<li><a href="{{ url('search') }}/album/{{ $search }}" class="albumSearch" >Albums</a></li>
-			        	<li><a href="{{ url('search') }}/user/{{ $search }}" class="userSearch" >Users</a></li>
+			        	<li><a href="#" class="songsSearch activePill" >Songs</a></li>
+			        	<li><a href="#" class="albumSearch" >Albums</a></li>
+			        	<li><a href="#" class="userSearch" >Users</a></li>
 			      	</ul>
 			    </div>
 		    </div>
