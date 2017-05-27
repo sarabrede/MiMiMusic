f9e45e05661e1669aef8701371baa66a4b799870
Sara Brede <sara_brede@hotmail.com>
2017-05-27 13:27:04 -0500

    Update songComponent.blade.php

--- resources/views/songComponent.blade.php
+++ resources/views/songComponent.blade.php
@@ -1,7 +1,7 @@
 @if( $idSong == $id)
 	<div class="col-xs-12 songContainer activeSong">
 		<div class="col-xs-9 nameSong">
-			<a href="{{ url('song') }}/{{ $id }}" target="_blank"> {{ $title }} </a>
+			<a href="{{ url('song') }}/{{ $id }}"> {{ $title }} </a>
 		</div>
 		<div class="col-xs-2 pull-right timeSong">
 			<p> 3:50 </p>
@@ -10,7 +10,7 @@
 @else
 	<div class="col-xs-12 songContainer ">
 		<div class="col-xs-9 nameSong">
-			<a href="{{ url('song') }}/{{ $id }}" target="_blank"> {{ $title }} </a>
+			<a href="{{ url('song') }}/{{ $id }}"> {{ $title }} </a>
 		</div>
 		<div class="col-xs-2 pull-right timeSong">
 			<p> 3:50 </p>
