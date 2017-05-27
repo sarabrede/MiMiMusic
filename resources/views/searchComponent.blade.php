8fad1eb768c5eb7c779ead3c28507d17e583918f
Sara Brede <sara_brede@hotmail.com>
2017-05-27 13:24:22 -0500

    Update searchComponent.blade.php

--- resources/views/searchComponent.blade.php
+++ resources/views/searchComponent.blade.php
@@ -1,19 +1,19 @@
-<div class ="col-xs-11 SearchResult">
-	<div class="col-xs-4 imageFrame">
-		<img src="{{ asset($image) }}" width="100%" height="100%" class="img-rounded"/>
+@if( $idSong == $id)
+	<div class="col-xs-12 songContainer activeSong">
+		<div class="col-xs-9 nameSong">
+			<a href="{{ url('song') }}/{{ $id }}"> {{ $title }} </a>
+		</div>
+		<div class="col-xs-2 pull-right timeSong">
+			<p> 3:50 </p>
+		</div>
 	</div>
-				
-	<div class="col-xs-5 infoResult">
-		<a href="{{ url('song') }}/{{ $id }}"> {{ $title }} </a> 
-		<br>
-		<a href="{{ url('album') }}/{{ $idAlbum }}"> {{ $album }} </a>
-		<br>
-		<label>{{ $genre }}</label>
-		<br>
-		<a href="{{ url('profile') }}/{{ $idUser }}"> {{ $author }} </a>
-		<p> {{ $description }} </p>
+@else
+	<div class="col-xs-12 songContainer ">
+		<div class="col-xs-9 nameSong">
+			<a href="{{ url('song') }}/{{ $id }}"> {{ $title }} </a>
+		</div>
+		<div class="col-xs-2 pull-right timeSong">
+			<p> 3:50 </p>
+		</div>
 	</div>
-	<div class="col-xs-2 infoResult pull-right text-center">
-		<p> {{ ($price > 0) ? '$'.$price : 'Free'}} </p>
-	</div>
-</div>
+@endif
